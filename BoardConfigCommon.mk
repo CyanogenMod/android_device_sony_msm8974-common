f4f777bfb43e6f5d729ba90d9d6fb425eca60e88
Chirayu Desai <chirayudesai1@gmail.com>
2014-11-13 19:37:25 +0530

    Initial commit of SELinux policy
    Change-Id: Id28e14f6cd09bc3f667bd35a2b71fd7f37560631

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#	  http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,3 +20,34 @@ BOARD_CUSTOM_BOOTIMG_MK := device/sony/msm8974-common/boot/custombootimg.mk
 BLUETOOTH_HCI_USE_MCT := true
 
 TARGET_USES_LOGD := false
+
+BOARD_SEPOLICY_DIRS += \
+	device/sony/msm8974-common/sepolicy
+
+# The list below is order dependent
+BOARD_SEPOLICY_UNION += \
+	app.te \
+	bluetooth.te \
+	bridge.te \
+	device.te \
+	domain.te \
+	file.te \
+	irsc_util.te \
+	mediaserver.te \
+	mpdecision.te \
+	netmgrd.te \
+	platform_app.te \
+	qmux.te \
+	radio.te \
+	rild.te \
+	rmt.te \
+	sensors.te \
+	surfaceflinger.te \
+	system_server.te \
+	tee.te \
+	time.te \
+	ueventd.te \
+	wpa.te \
+	file_contexts \
+	genfs_contexts \
+	te_macros
