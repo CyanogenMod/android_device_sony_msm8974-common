aaeebcbf9eff015f95e91ef37c37778a8a0be776
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-29 15:58:19 +0100

    more msm8974 consolidation
    Change-Id: I1e6bd6cae183670ef20f95d832911eca85b55dbf

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -12,15 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# inherit from Sony common
+include device/sony/common/BoardConfigCommon.mk
+
+# must be defined before qcom-common include
+BOARD_EGL_CFG := device/sony/shinano-common/rootdir/system/lib/egl/egl.cfg
+
 # inherit from qcom-common
 include device/sony/qcom-common/BoardConfigCommon.mk
 
-BOARD_CUSTOM_BOOTIMG_MK := device/sony/msm8974-common/boot/custombootimg.mk
+# Kernel properties
+TARGET_KERNEL_SOURCE := kernel/sony/msm8974
 
-TARGET_USES_LOGD := false
+# use CAF variants
+BOARD_USES_QCOM_HARDWARE := true
+
+# Platform
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+TARGET_BOARD_PLATFORM := msm8974
+
+# Architecture
+TARGET_CPU_VARIANT := krait
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+AUDIO_FEATURE_DISABLED_USBAUDIO := true
 
 # Bionic
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 BOARD_USES_LEGACY_MMAP := true
+
+# Graphics
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+
+BOARD_CUSTOM_BOOTIMG_MK := device/sony/msm8974-common/boot/custombootimg.mk
+
+# Logd
+TARGET_USES_LOGD := false
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
@@ -42,3 +84,6 @@ BOARD_SEPOLICY_UNION += \
     wpa.te \
     file_contexts \
     genfs_contexts
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
