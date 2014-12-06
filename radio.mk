07f28d261bfa03b3d2498eee92ebd170df72dda5
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-12-06 02:47:36 +0100

    update ril prop
    Change-Id: I9ab6e77c21c69c6aa4a3fc1cfe40c92c05733bde

--- radio.mk
+++ radio.mk
@@ -4,9 +4,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     telephony.lteOnCdmaDevice=0 \
     persist.radio.apm_sim_not_pwdn=1 \
     persist.radio.add_power_save=1 \
-    persist.radio.dont_use_dsd=false \
-    persist.radio.eons.enabled=false \
-    persist.radio.oem_socket=false
+    persist.radio.oem_socket=true \
+    persist.radio.data_no_toggle=1 \
+    persist.radio.calls.on.ims=0
 
 # Ril sends only one RIL_UNSOL_CALL_RING, so set call_ring.multiple to false
 PRODUCT_PROPERTY_OVERRIDES += \
