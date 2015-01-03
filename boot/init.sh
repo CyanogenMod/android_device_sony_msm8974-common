bb0411d84de5cfc9ee5199dbe77d183777bd23ec
Olivier K <olivier@olivierk.eu>
2015-01-03 17:52:08 +0100

    init: Support LZMA-compressed recovery ramdisks
    * If everything else fails, use LZMA.
    * Remove useless symlink
    
    Props to [NUT]
    
    Change-Id: I706b938c1fd3a2b7b7d167da95c66b3231de9294

--- boot/init.sh
+++ boot/init.sh
@@ -50,9 +50,14 @@ if [ $? -eq 42 ] || busybox grep -q warmboot=0x77665502 /proc/cmdline ; then
 	# recovery ramdisk
 	busybox mknod -m 600 ${BOOTREC_FOTA_NODE}
 	busybox mount -o remount,rw /
-	busybox ln -sf /sbin/busybox /sbin/sh
 	extract_elf_ramdisk -i ${BOOTREC_FOTA} -o /sbin/ramdisk-recovery.cpio -t / -c
-	busybox rm /sbin/sh
+	if [ $? -eq 255 ]; then
+		# Try again!
+		busybox echo 'Unable to extract FOTAKernel ramdisk! Retrying...' >>boot.txt
+		extract_elf_ramdisk -i ${BOOTREC_FOTA} -o /sbin/ramdisk-recovery.cpio -t / -d
+		busybox mv /sbin/ramdisk-recovery.cpio /sbin/ramdisk-recovery.cpio.lzma
+		busybox lzma -d /sbin/ramdisk-recovery.cpio.lzma
+	fi
 	load_image=/sbin/ramdisk-recovery.cpio
 else
 	busybox echo 'ANDROID BOOT' >>boot.txt
