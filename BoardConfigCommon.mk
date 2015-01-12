c8c9141615700f51fe54eb6d07f24283bc039b9f
Chirayu Desai <chirayudesai1@gmail.com>
2015-01-12 19:08:15 +0530

    camera: Add SELinux policy
    Change-Id: I359dd389bbf772ab5370e5270377021d2d8c458a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,7 +85,10 @@ BOARD_SEPOLICY_UNION += \
     file.te \
     location.te \
     mac-update.te \
+    mediaserver.te \
+    mm-qcamerad.te \
     mpdecision.te \
+    property.te \
     radio.te \
     rild.te \
     sct.te \
@@ -99,7 +102,8 @@ BOARD_SEPOLICY_UNION += \
     time_daemon.te \
     wpa.te \
     file_contexts \
-    genfs_contexts
+    genfs_contexts \
+    property_contexts
 
 # Time
 BOARD_USES_QC_TIME_SERVICES := true
