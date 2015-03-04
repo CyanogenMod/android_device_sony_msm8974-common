dc94cf7587532bf30c6e8c75b1f0ec2e7b125635
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-04 12:20:03 +0100

    Import adreno driver from qcom-common
    Change-Id: Ib5ea08d586474f35ccfd17f27a39c5265960d5bf

---
+++ extract-files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Use tradition sort
+export LC_ALL=C
+
+FP=$(cd ${0%/*} && pwd -P)
+export VENDOR=$(basename $(dirname $FP))
+export DEVICE=$(basename $FP)
+export BOARDCONFIGVENDOR=false
+
+../common/extract-files.sh $@
+
+../common/setup-makefiles.sh
