f2ab3b273d8f19f4a8c7bfeab6bcd4c031971353
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-03-04 12:20:03 +0100

    import opengl/adreno from qcom-common
    Change-Id: Ib5ea08d586474f35ccfd17f27a39c5265960d5bf

--- msm8974.mk
+++ msm8974.mk
@@ -87,3 +87,10 @@ endif
 # Thermal management
 PRODUCT_PACKAGES += \
     thermanager
+
+# OpenGL ES 3.0
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=196608
+
+# Include non-opensource parts
+$(call inherit-product, vendor/sony/msm8974-common/msm8974-common-vendor.mk)
