f328d4894e408a13570c257be570bf6698e83356
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-04-10 21:08:51 +0200

    replace libqc-opt with libqti-perfd-client
    Change-Id: I14985972c7f879b324bf7e089ff539627822950c

--- msm8974.mk
+++ msm8974.mk
@@ -113,7 +113,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+    ro.vendor.extension_library=libqti-perfd-client.so
 
 # Recovery
 PRODUCT_PACKAGES += \
