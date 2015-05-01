3fc6f8795067eefeb1d8af22938333c7d7981f84
Chirayu Desai <chirayudesai1@gmail.com>
2015-05-01 10:12:15 +0530

    Make sure thermanager is present
    * This is not a default repo, and might not be present in some
      people's local manifests. Happened with me too for a while :(
    * Include a dummy makefile to make sure it is present,
      and error out if not.
    
    Change-Id: I5322ffc138d41e4aef39998c603cba8db63afb6c

--- Android.mk
+++ Android.mk
@@ -3,5 +3,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),sony)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8974)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
+    include hardware/sony/thermanager/dummy.mk
 endif
 endif
