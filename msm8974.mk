eff108e505fabe8ac457e5f895e740bda8708bfa
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2015-06-24 16:16:30 +0200

    msm8974: Don't need to copy FFMPEG config anymore
    Change-Id: Icecf5657bdbe5b187a4c9c7c1dc56c733fdb2e8c

--- msm8974.mk
+++ msm8974.mk
@@ -68,7 +68,6 @@ PRODUCT_PACKAGES += \
 
 # Media profile
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
@@ -134,10 +133,6 @@ PRODUCT_PACKAGES += \
     dhcpcd.conf \
     wpa_supplicant \
     wpa_supplicant.conf
-
-# QCOM Display
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.hwc.mdpcomp.enable=true
 
 # OpenGL ES 3.0
 PRODUCT_PROPERTY_OVERRIDES += \
