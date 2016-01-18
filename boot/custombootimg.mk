81ce042d04a0209e5e9f2a8556fb6ad2090592e6
Chirayu Desai <chirayudesai1@gmail.com>
2016-01-18 12:16:59 +0530

    Remove bootlogo copy step
    * Provided by the bootloader since the last few releases.
    
    Change-Id: I21d12742fc9c2338a2d40d59c91b6a02f50a573f

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -19,7 +19,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(r
 	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
 	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
 
-	$(hide) mv $(PRODUCT_OUT)/root/logo.rle $(PRODUCT_OUT)/combinedroot/logo.rle
 	$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
 	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/combinedroot/sbin/
