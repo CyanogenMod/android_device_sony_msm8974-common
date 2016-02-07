2435839b7afd5c27b37498a5641d2dbd46c547ab
Chirayu Desai <chirayudesai1@gmail.com>
2016-02-07 16:03:28 +0530

    Switch to AOSP proprietary files
    * SW_binaries_for_Xperia_AOSP_M_v02a
    * Generated using https://github.com/fxpdev/vendor-fxp
    
    Change-Id: I059adcfc64ea8cbdf548bdd8d454e0a36b61c7ee

--- extract-files.sh
+++ extract-files.sh
@@ -6,8 +6,10 @@ export LC_ALL=C
 FP=$(cd ${0%/*} && pwd -P)
 export VENDOR=$(basename $(dirname $FP))
 export DEVICE=$(basename $FP)
+export TARGET_BOARD_PLATFORM=msm8974
 export BOARDCONFIGVENDOR=false
 
 ../common/extract-files.sh $@
 
 ../common/setup-makefiles.sh
+./setup-makefiles.sh
